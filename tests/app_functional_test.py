eac42387133ed4cfd3912bf87ae83105ea8af0a9
Marcus Parker <marcuseparker93@gmail.com>
2022-01-29 21:53:42 +0000

    Added user story to FT, setUp and tearDown added for unit tests.

--- tests/app_functional_test.py
+++ tests/app_functional_test.py
@@ -1,9 +1,29 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_home_page_includes_hello_world_in_html_body():
-    browser = webdriver.Firefox()
-    browser.get('http://localhost:5000/')
-    body_text = browser.find_element(By.TAG_NAME, 'body')
+class NewVisitorTest(unittest.TestCase):
 
-    assert 'Hello, World!' in body_text.text
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+       self.browser.quit()
+
+    def test_home_page_includes_hello_world_in_html_body(self):
+        self.browser.get('http://localhost:5000/')
+        body_text = self.browser.find_element(By.TAG_NAME, 'body')
+
+        self.assertIn('Hello, World!', body_text.text)
+
+    # User has stumbled upon this great online blogging app
+    # They go checkout its homepage
+
+    # They realise the title and header mention blogging app
+
+    # They also realise the posts of members with accounts on the left half 
+    # of the page
+
+    # They are prompted to login if they have an account or create a new account
+
+
--- tests/app_unit_test.py
+++ tests/app_unit_test.py
@@ -1,14 +1,28 @@
+import pytest
 from pathlib import Path
 
 from project.app import app, init_db
 
-def test_index_page_gives_200_response():
-    tester = app.test_client()
-    response = tester.get('/', content_type='html/text')
+TEST_DB = 'test.db'
 
+
+@pytest.fixture
+def set_up_test_client():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    app.config['TESTING'] = True
+    app.config['DATABASE'] = BASE_DIR.joinpath(TEST_DB)
+
+    init_db()  # setUp 
+    yield app.test_client()
+    init_db()  # tearDown
+
+def test_index_page_gives_200_response(set_up_test_client):
+    response = set_up_test_client.get('/', content_type='html/text')
     assert response.status_code == 200
-    assert response.data == b"Hello, World!"
 
-def test_database_exists():
-    init_db()
-    assert Path('flaskr.db').is_file()
+def test_database_exists(set_up_test_client):
+    assert Path('test.db').is_file()
+
+def test_database_is_empty_when_page_is_first_loaded(set_up_test_client):
+    db_table = set_up_test_client.get('/')
+    assert b'No entries yet. Add some!' in db_table.data
