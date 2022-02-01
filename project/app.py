a05c658e7343c649ba04a0c59c3d696b4be86fed
Marcus Parker <marcuseparker93@gmail.com>
2022-02-01 22:23:53 +0000

    Added all views, next will add more features.

--- project/app.py
+++ project/app.py
@@ -4,7 +4,7 @@ import urllib
 import datetime
 import functools
 
-from flask import (Flask, Response, flash, session, request,
+from flask import (Flask, Response, abort, flash, session, request,
                     redirect, render_template, url_for, Markup)
 
 from micawber.cache import Cache as OEmbedCache
@@ -24,6 +24,7 @@ APP_DIR = os.path.dirname(os.path.realpath(__file__))
 DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
 DEBUG = False
 SECRET_KEY = 'dev'
+SITE_WIDTH = 800
 
 # Initialize app
 app = Flask(__name__)
@@ -47,54 +48,6 @@ class Entry(flask_db.Model):
     published = BooleanField(index=True)
     timestamp = DateTimeField(default=datetime.datetime.now, index=True)
 
-    # Handles displaying published blog posts
-    @classmethod
-    def public(cls):
-        return Entry.select().where(Entry.published == True)
-
-    # Handles searching
-    @classmethod
-    def search(cls, query):
-        words = [word.strip() for word in query.split() if word.strip()]
-        if not words:
-            # Return empty query search
-            return Entry.select().where(Entry.id == 0)
-        else:
-            search = ' '.join(words)
-        return (Entry
-                .select(Entry, FTSEntry.rank().alias('score'))
-                .join(FTSEntry, on=(Entry.id == FTSEntry.docid))
-                .where(
-                    (Entry.published == True) &
-                    (FTSEntry.match(search)))
-                .order_by(SQL('score')))
-    
-    # Handles displaying drafted blog posts
-    @classmethod
-    def drafts(cls):
-        return Entry.select().where(Entry.published == False)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            # Example: title: 'Blog Title', slug: blog-title'
-            self.slug = re.sub('[^\w]+', '-', self.title.lower())
-        re_entry = super(Entry, self).save(*args, **kwargs)
-
-        # Store in search content
-        self.update_search_index()
-        return re_entry
-
-    # Adds new entry to full text search table
-    def update_search_index(self):
-        search_content = '\n'.join((self.title, self.content))
-        try:
-            fts_entry = FTSEntry.get(FTSEntry.docid == self.id)
-        except FTSEntry.DoesNotExist:
-            FTSEntry.create(docid=self.id, content=search_content)
-        else:
-            fts_entry.content = search_content
-            fts_entry.save()
-
     # Converts markdown format into HTML and media objects into embedded objects
     @property
     def html_content(self):
@@ -109,10 +62,67 @@ class Entry(flask_db.Model):
         )
         return Markup(oember_content)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Example: title: 'Blog Title', slug: blog-title'
+            self.slug = re.sub('[^\w]+', '-', self.title.lower()).strip('-')
+        re_entry = super(Entry, self).save(*args, **kwargs)
+
+        # Store in search content
+        self.update_search_index()
+        return re_entry
+
+    # Adds new entry to full text search table
+    def update_search_index(self):
+        exists = (FTSEntry
+                    .select(FTSEntry.docid)
+                    .where(FTSEntry.docid == self.id)
+                    .exists()
+                )
+        content = '\n'.join((self.title, self.content))
+        if exists:
+            (FTSEntry
+                .update({FTSEntry.content: content})
+                .where(FTSEntry.docid == self.id)
+                .execute()
+            )
+        else:
+            FTSEntry.insert({
+                FTSEntry.docid: self.id,
+                FTSEntry.content: content
+            }).execute()
+
+    # Handles displaying published blog posts
+    @classmethod
+    def public(cls):
+        return Entry.select().where(Entry.published == True)
+    
+    # Handles displaying drafted blog posts
+    @classmethod
+    def drafts(cls):
+        return Entry.select().where(Entry.published == False)
+
+    # Handles searching
+    @classmethod
+    def search(cls, query):
+        words = [word.strip() for word in query.split() if word.strip()]
+        if not words:
+            # Return empty query search
+            return Entry.noop()
+        else:
+            search = ' '.join(words)
+        return (Entry
+                .select(Entry, FTSEntry.rank().alias('score'))
+                .join(FTSEntry, on=(Entry.id == FTSEntry.docid))
+                .where(
+                    FTSEntry.match(search) &
+                    (Entry.published == True))
+                .order_by(SQL('score')))
+
 
 # Handles search database
 class FTSEntry(FTSModel):
-    content = SearchField()
+    content = TextField()
 
     
     class Meta:
@@ -161,32 +171,44 @@ def index():
         query = Entry.search(search_query)
     else:
         query = Entry.public().order_by(Entry.timestamp.desc())
-    return object_list('index.html', query, search=search_query)
+    return object_list(
+        'index.html',
+        query,
+        search=search_query,
+        check_bounds=False
+        )
+
+def create_or_edit(entry, template):
+    if request.method == 'POST':
+        entry.title = request.form.get('title') or ''
+        entry.content = request.form.get('content') or ''
+        entry.published = request.form.get('published') or False
+        if not (entry.title or entry.content):
+            flash('Title and content are required.', 'danger')
+        else:
+            try:
+                with database.atomic():
+                    entry.save()
+            except IntegrityError:
+                flash('Error: this title is already in use.', 'danger')
+            else:
+                flash('Post succesfully created.', 'success')
+                if entry.published:
+                    return redirect(url_for('detail', slug=entry.slug))
+                else:
+                    return redirect(url_for('edit', slug=entry.slug))
+    return render_template(template, entry=entry)
+
+@app.route('/create/', methods=['GET', 'POST'])
+@login_required
+def create():
+    return create_or_edit(Entry(title='', content=''), 'create.html')
 
 @app.route('/drafts/')
 @login_required
 def drafts():
     query = Entry.drafts().order_by(Entry.timestamp.desc())
     return object_list('index.html', query)
-
-@app.route('/create/', methods=['GET', 'POST'])
-@login_required
-def create():
-    if request.method == 'POST':
-        if request.form.get('title') and request.form.get('content'):
-            entry = Entry.create(
-                title=request.form['title'],
-                content=request.form['content'],
-                published=request.form.get('published') or False
-            )
-            flash('Post submitted.', 'success')
-            if entry.published:
-                return redirect(url_for('detail', slug=entry.slug))
-            else:
-                return redirect(url_for('edit', slug=entry.slug))
-        else:
-            flash('Title and content are required.', 'danger')
-    return render_template('create.html')
 
 @app.route('/<slug>/')
 def detail(slug):
@@ -202,21 +224,7 @@ def detail(slug):
 @login_required
 def edit(slug):
     entry = get_object_or_404(Entry, Entry.slug == slug)
-    if request.method == 'POST':
-        if request.form.get('title') and request.form.get('content'):
-            entry.title = request.form['title']
-            entry.content = request.form['content']
-            entry.published = request.form.get('published') or False
-            entry.save()
-
-            flash('Post has been edited.', 'success')
-            if entry.published:
-                return redirect(url_for('detail', slug=entry.slug))
-            else:
-                return redirect(url_for('edit', slug=entry.slug))
-        else:
-            flash('Title and content are required.', 'danger')
-    return render_template('edit.html')
+    return create_or_edit(entry, 'edit.html')
             
 # Helper functions
 
@@ -229,7 +237,7 @@ def clean_querystring(request_args, *keys_to_remove, **new_values):
     return urllib.urlencode(querystring)
 
 @app.errorhandler(404)
-def not_found(err):
+def not_found(exc):
     return Response('<h3>Not found</h3>'), 404
 
 def main():
